868709fada07f3dde1041550dc2f22ee3275e235
sethtara <yeharuto@gmail.com>
2023-03-27 21:58:14 +0545

    changes in constants.js

--- src/components/constants.js
+++ src/components/constants.js
@@ -5,7 +5,7 @@ import CityIds from './CityId';
 export const apiUrl=(cityId)=> `http://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=${process.env.REACT_APP_API_KEY}&units=metric`;
 export const cityCodes = CityIds();
 export const colors = ["#388EE7", "#8B49CC", "#3C882C", "#C63F3F", "#6249CC", "#1B127B", "#D847DB", "#138E78"];
-export const cacheTime = 0 * 60 * 1000; //five minutes
+export const cacheTime = 5 * 60 * 1000; //five minutes
 export const degCelcius="ºc";
 export const formatTime=(value)=>value.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
 export const formatDate=(value)=>value.toLocaleDateString('en-US', { month: 'long', day: 'numeric' });
