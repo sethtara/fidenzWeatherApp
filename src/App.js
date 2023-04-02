1c3456729c2bf3c687977f97068b541d877e7174
sethtara <yeharuto@gmail.com>
2023-04-02 07:18:51 +0545

    fixing unwanted loops

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import './css/App.css';
 import WeatherCard from "./components/WeatherCard"
 import * as CONST from './utils/constants';
+import weatherData from "./data/cities.json"
 
 function App() {
-
-  const cityCodes = CONST.cityCodes;
-
-  const weatherCards = cityCodes.map((cityId, index) => (
-    <WeatherCard key={cityId} cityId={cityId} bgColor={CONST.colors[index % CONST.colors.length]}/>
-  ));
+  const weatherCards = weatherData.List.map((city, index) => (
+    <WeatherCard key={city.CityCode} cityId={city.CityCode} bgColor={CONST.colors[index % CONST.colors.length]}/>
+    ));
 
   return (
     <div className="row App weather_cards">
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,12 +1,7 @@
-import weatherData from "../data/cities.json"
-
-
 //Constants used in the app
 
-export const CityIds=()=>weatherData.List.map(city => city.CityCode);
 export const apiUrl=(cityId)=> `http://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=${process.env.REACT_APP_API_KEY}&units=metric`;
 
-export const cityCodes = CityIds();
 
 export const colors = ["#388EE7", "#8B49CC", "#3C882C", "#C63F3F", "#6249CC", "#1B127B", "#D847DB", "#138E78"];
 
