1a1a90d7025c3495bc232f695aea0622782d785b
sethtara <yeharuto@gmail.com>
2023-04-05 09:12:25 +0545

    Assignment 2 Auth0 implimentations

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 import "./css/App.css";
 import WeatherCard from "./components/WeatherCard";
 import * as CONST from "./utils/constants";
 import weatherData from "./data/cities.json";
 
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from "react-bootstrap/Button";
+
+function NavBar() {
+  const { user, loginWithRedirect, isAuthenticated, logout } = useAuth0();
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home"> <img src={`assets/img/logo.png`} alt="logo" /></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+          
+          </Nav>
+          <Nav>
+          <Navbar.Collapse className="justify-content-end">
+            {isAuthenticated && (
+              <Navbar.Text className="userName">
+                Signed in as : {user.nickname}
+              </Navbar.Text>
+            )}
+            
+            {isAuthenticated ? (
+              <Button variant="danger" onClick={logout}>
+                Logout
+              </Button>
+            ) : (
+              <Button variant="primary" onClick={loginWithRedirect}>
+                login
+              </Button>
+            )}
+
+          </Navbar.Collapse>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+
 function App() {
+  const {isAuthenticated } = useAuth0();
+
   const weatherCards = weatherData.List.map((city, index) => (
     <WeatherCard
       key={city.CityCode}
@@ -13,6 +59,36 @@ function App() {
     />
   ));
 
-  return <div className="row App weather_cards">{weatherCards}</div>;
+  return (
+    <>
+      <NavBar></NavBar>
+
+      <div className="container contents">
+        <div className="row">
+          <div className="input-group search-box">
+            <input
+              type="search"
+              id="search_input"
+              className="form-control "
+              placeholder="Enter a city"
+              aria-label="Search"
+              aria-describedby="search-addon"
+            />
+            <button type="button" id="search_button" className="btn">
+              Add City
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="container">
+        { isAuthenticated &&(
+            <div className="row App weather_cards">{weatherCards}</div>
+
+          )
+        }
+      </div>
+    </>
+  );
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import "react-bootstrap/dist/react-bootstrap"
+import { Auth0Provider } from "@auth0/auth0-react";
+//import "bootstrap/dist/css/bootstrap.min.css"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+      <Auth0Provider
+   domain="aztenant.eu.auth0.com"
+   clientId="kTRilIk3U0ldcQMTL08Z8TyvWHC28gka"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
+  </Auth0Provider>
   </React.StrictMode>
 );
 
